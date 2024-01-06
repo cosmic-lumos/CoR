38b32d0788cd8b31e1d1333509b0c8c893cc34b6
BiteSnail <rkwoal216@gmail.com>
2024-01-06 17:11:36 +0900

    feat: websocket 테스트

--- coroulette/src/main/java/com/cosmic/coroulette/CorouletteApplication.java
+++ coroulette/src/main/java/com/cosmic/coroulette/CorouletteApplication.java
@@ -9,5 +9,4 @@ public class CorouletteApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CorouletteApplication.class, args);
 	}
-	
 }
---
+++ coroulette/src/main/java/com/cosmic/coroulette/controller/GreetingController.java
@@ -0,0 +1,20 @@
+package com.cosmic.coroulette.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+import com.cosmic.coroulette.dao.HelloMessage;
+import com.cosmic.coroulette.dao.Greeting;
+
+@Controller
+public class GreetingController {
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000);
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+}
