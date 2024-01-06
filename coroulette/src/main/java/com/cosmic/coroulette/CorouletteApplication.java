9f633123cc9360d8985462faa076d4d9116b788e
BiteSnail <rkwoal216@gmail.com>
2024-01-06 16:36:14 +0900

    init: 스프링 부트 프로젝트 시작 커밋

---
+++ coroulette/src/main/java/com/cosmic/coroulette/CorouletteApplication.java
@@ -0,0 +1,13 @@
+package com.cosmic.coroulette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorouletteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorouletteApplication.class, args);
+	}
+	
+}
---
+++ coroulette/src/test/java/com/cosmic/coroulette/CorouletteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cosmic.coroulette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorouletteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
